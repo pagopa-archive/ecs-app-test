eff20d0dac9ced13bb9d486235500355dca81083
uolter <walter.traspadini@pagopa.it>
2022-03-07 12:16:44 +0100

    go app changed output json api

--- src/webapp/app.go
+++ src/webapp/app.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
 const (
-	VERSION = 2
+	VERSION int    = 3
+	port    string = "8000"
 )
 
-func hostnameHandler(w http.ResponseWriter, r *http.Request) {
-	myhostname, _ := os.Hostname()
-	fmt.Fprintln(w, "Hostname:", myhostname, " Version:", VERSION)
+type Citizen struct {
+	Id         string
+	CF         string
+	FirstName  string
+	LastName   string
+	ApiVersion int
+}
+
+var Citizens = []Citizen{
+	{Id: "001", FirstName: "Jhon", LastName: "Smith", CF: "MRTMTT91D08F205J", ApiVersion: VERSION},
+	{Id: "002", FirstName: "Robert", LastName: "De Niro", CF: "MLLSNT82P65Z404U", ApiVersion: VERSION},
+}
+
+func getAllCitizens(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Citizens)
+}
+
+func handleRequests() {
+
+	http.HandleFunc("/", getAllCitizens)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func main() {
-	const port string = "8000"
-	fmt.Println("Server listening on port", port)
-	http.HandleFunc("/", hostnameHandler)
-	http.ListenAndServe(":"+port, nil)
+	fmt.Println(fmt.Sprintf("Rest API v%d - Mux Routers listening on port %s", VERSION, port))
+
+	handleRequests()
 }
